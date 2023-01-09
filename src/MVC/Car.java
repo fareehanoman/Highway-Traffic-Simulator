97e707d1e75f7e1b39f64d894a7c13b5f69a49ad
fareehanoman <116712554+fareehanoman@users.noreply.github.com>
2023-01-09 21:09:25 +0500

    Update Car.java
    comments added

--- src/MVC/Car.java
+++ src/MVC/Car.java
@@ -7,7 +7,9 @@ import java.awt.Point;
 public class Car extends Vehicles {
 
     public Car(int speed, int delay, Driver driver,Point start, Modelable theModel) {
+		//super() is used to call parent class constructor
         super(speed, delay,driver,start, theModel);
+		//car dimensions
         length = 45;
         width = 25;
         wheelSize = 6;
@@ -15,11 +17,14 @@ public class Car extends Vehicles {
     }
 
     public void paint(Graphics g, int start) {
-        LaneChanger();
-
+        LaneChanger();//method in Vehicles that tells when to change lane
+        //car color is red
         g.setColor(Color.red);
+		//dimensions of red car
         g.fillRect(position.x-start, position.y, length, width);
+		//car tyre color is black
         g.setColor(Color.black);
+		//wheel dimensions of car at x and y coordinates
         g.fillOval(position.x-start + length/7, position.y - wheelSize*4/5, wheelSize, wheelSize);
         g.fillOval(position.x-start + length* 5/7, position.y - wheelSize*4/5, wheelSize, wheelSize);
         g.fillOval(position.x-start + length/7, position.y + width -  wheelSize*1/5, wheelSize, wheelSize);
