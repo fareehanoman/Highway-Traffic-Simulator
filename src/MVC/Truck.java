7b2325dee25ab4eeb9dbec461e47a0c23d7233e8
fareehanoman <116712554+fareehanoman@users.noreply.github.com>
2023-01-09 21:32:39 +0500

    subclass->Vehicles

--- src/MVC/Truck.java
+++ src/MVC/Truck.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package MVC;
 
 import java.awt.Graphics;
 import java.awt.Point;
 
-/**
- *
- * @author christopherbarnes
- */
 public class Truck extends Vehicles{
      public Truck(int speed, int delay, Driver driver,Point start, Modelable theModel) {
         super(speed, delay,driver,start, theModel);
@@ -24,7 +15,6 @@ public class Truck extends Vehicles{
 
     public void paint(Graphics g, int start) {
         LaneChanger();
-
         g.drawImage(loader.img, position.x-start, position.y-5, length, width, null);
     }
 }
