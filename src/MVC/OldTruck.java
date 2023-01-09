ca42ee871df7d750a6f72bd9fa20e02d93647443
fareehanoman <116712554+fareehanoman@users.noreply.github.com>
2023-01-09 21:30:37 +0500

    comments added

--- src/MVC/OldTruck.java
+++ src/MVC/OldTruck.java
@@ -1,30 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package MVC;
-
 import java.awt.Graphics;
 import java.awt.Point;
 
-/**
- *
- * @author Ari
- */
 public class OldTruck extends Vehicles {
 
     public OldTruck(int speed, int delay, Driver driver, Point start, Modelable theModel) {
         super(speed, delay,driver,start, theModel);
+        //truck dimensions
         length = 90;
         width = 45;
+        //truck speed
         topSpeed = 50;
+        //load truck image
         loader = new LoadImage("truck.png");
     }
 
     @Override
     void paint(Graphics g, int start) {
         LaneChanger();
+        //The method paint() gives us access to an object of type Graphics class 
         g.drawImage(loader.img, position.x - start, position.y-7, length, width, null);
     }
 
