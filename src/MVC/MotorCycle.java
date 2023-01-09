c9dfc8b8439766773ce1fff9db6414eff345e986
fareehanoman <116712554+fareehanoman@users.noreply.github.com>
2023-01-09 21:22:30 +0500

    comments added

--- src/MVC/MotorCycle.java
+++ src/MVC/MotorCycle.java
@@ -1,34 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package MVC;
 
 import java.awt.Graphics;
 import java.awt.Point;
-
-/**
- *
- * @author Ari
- */
 public class MotorCycle extends Vehicles {
-
-   
     
     public MotorCycle(int speed, int delay, Driver driver, Point start,Modelable theModel) {
         super(speed, delay,driver,start, theModel);
+        //the length and width of motorcycle 
         length = 30;
         width = 15;
         loader = new LoadImage("motorcycle.png");
+        //speed of motorcycle
         topSpeed = 80;
     }
 
     @Override
     void paint(Graphics g, int start) {
         LaneChanger();
-        
         g.drawImage(loader.img, position.x-start, position.y+10, length, width, null);   
     }
-    
 }
