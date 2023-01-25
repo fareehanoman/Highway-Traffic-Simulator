11cee9a81e0be05fd17a83400700acb1b2d4756b
fareehanoman <116712554+fareehanoman@users.noreply.github.com>
2023-01-25 10:19:20 +0500

    comments added

--- src/MVC/Modelable.java
+++ src/MVC/Modelable.java
@@ -1,23 +1,14 @@
 package MVC;
-
 import java.util.ArrayList;
 
 /**
- * The interface your model will implement
+ * The interface our model will implement
  * Add all the the methods that will be sent to the Model
- * 
- * @author levenick
  */
 public interface Modelable {
     public void step();
     public void init();
-    
     public int getT();
     
     public VehicleList viewSees(int begin, int end);
-
-    
-    
-    
-    
 }
