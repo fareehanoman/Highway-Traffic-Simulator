dd5f0048d5958e6a0c3e782bf2d6702c778b5974
fareehanoman <116712554+fareehanoman@users.noreply.github.com>
2023-01-25 10:21:05 +0500

    interface

--- src/MVC/Modelable.java
+++ src/MVC/Modelable.java
@@ -5,10 +5,11 @@ import java.util.ArrayList;
  * The interface our model will implement
  * Add all the the methods that will be sent to the Model
  */
+
 public interface Modelable {
     public void step();
     public void init();
     public int getT();
-    
+//all the vehicles in VehicleList are called    
     public VehicleList viewSees(int begin, int end);
 }
