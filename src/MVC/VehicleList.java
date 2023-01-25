62894178743029e9227afc842318c70b2aeca1ba
fareehanoman <116712554+fareehanoman@users.noreply.github.com>
2023-01-25 10:17:33 +0500

    comments added

--- src/MVC/VehicleList.java
+++ src/MVC/VehicleList.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package MVC;
-
 import java.util.ArrayList;
 
-/**
- *
- * @author christopherbarnes
- */
+// An array list that maintains all the vehicles
 class VehicleList extends ArrayList<Vehicles> {
     
 }
